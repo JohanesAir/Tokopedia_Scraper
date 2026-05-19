095a8fea0a98375827c20fb4931ded71f3ac48bd
JohanesAir <johanes8989@gmail.com>
2026-05-19 15:28:58 +0700

    Add scroll func to get new query api

--- main.go
+++ main.go
@@ -45,7 +45,8 @@ type Response struct {
 }
 
 func main() {
-
+	var products []Product
+	var result []Wrapper
 	pw, err := playwright.Run()
 	if err != nil {
 		log.Fatal(err)
@@ -83,6 +84,30 @@ func main() {
 				}
 
 				dataChannel <- string(body)
+
+				select {
+				case data := <-dataChannel:
+					fmt.Println("DATA RECEIVED SIZE:", len(data))
+
+					err := json.Unmarshal([]byte(data), &result)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					if len(result) != 0 {
+						products = append(products, result[0].Data.SearchProductV5.Data.Products...)
+						fmt.Println("🔥 Tambah data")
+						fmt.Println("TOTAL:", len(products))
+					}
+
+					if len(products) < 100 {
+						page.Goto("https://www.tokopedia.com/search?ob=5&related=true&srp_component_id=04.06.00.00&st=product&q=handphone")
+						time.Sleep(3000 * time.Millisecond)
+					}
+
+				case <-time.After(15 * time.Second):
+					log.Fatal("timeout waiting graphql")
+				}
 			}()
 		}
 	})
@@ -95,61 +120,40 @@ func main() {
 		},
 	)
 	page.Goto("https://www.tokopedia.com/search?ob=5&related=true&srp_component_id=04.06.00.00&st=product&q=handphone")
-
-	// WAIT DATA SAFE
-	select {
-	case data := <-dataChannel:
-		fmt.Println("DATA RECEIVED SIZE:", len(data))
-
-		var result []Wrapper
-
-		err := json.Unmarshal([]byte(data), &result)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if len(result) == 0 {
-			log.Fatal("empty response")
-		}
-
-		products := result[0].Data.SearchProductV5.Data.Products
-
-		// CREATE CSV
-		file, err := os.Create("products.csv")
-		if err != nil {
-			log.Fatal("could not create file:", err)
-		}
-		defer file.Close()
-
-		writer := csv.NewWriter(file)
-		defer writer.Flush()
-		writer.Write([]string{
-			"Product Name",
-			"Description",
-			"Image Link",
-			"Price",
-			"Rating",
-			"Store",
-		})
-
-		// WRITE CSV
-		fmt.Println("TOTAL:", len(products))
-
-		for i := 0; i < len(products) && i < 100; i++ {
-			p := products[i]
-			writer.Write([]string{
-				p.Name,
-				p.Description,
-				p.MediaURL.Image,
-				p.Price.Text,
-				p.Rating,
-				p.Shop.Name,
-			})
-		}
-
-	case <-time.After(15 * time.Second):
-		log.Fatal("timeout waiting graphql")
+	time.Sleep(3000 * time.Millisecond)
+	// CREATE CSV
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatal("could not create file:", err)
 	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	writer.Write([]string{
+		"Product Name",
+		"Description",
+		"Image Link",
+		"Price",
+		"Rating",
+		"Store",
+	})
+
+	// WRITE CSV
+
+	for i := 0; i < len(products) && i < 100; i++ {
+		p := products[i]
+		writer.Write([]string{
+			p.Name,
+			p.Description,
+			p.MediaURL.Image,
+			p.Price.Text,
+			p.Rating,
+			p.Shop.Name,
+		})
+	}
+
 	fmt.Println("CSV CREATED")
 	fmt.Println("DONE XD")
+	fmt.Scanln()
 }
